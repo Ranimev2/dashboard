a1e75bf5e2f0a12e5cc78d2453973c4c36bd825e
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-08-11 09:34:28 +0700

    Create server.js

---
+++ billing/server.js
@@ -0,0 +1,4 @@
+// Add event listener to the button
+document.querySelector('.premium-button').addEventListener('click', function() {
+  alert('You have successfully subscribed to the premium billing server!');
+});
